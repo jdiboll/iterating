59a0906c1b751208758387f100a4daa23a21d93e
FIRST_NAME LAST_NAME <YOUR_EMAIL>
2016-03-02 23:43:26 -0500

    complete
    excersises complete with questions and issues..

--- exercise_2.js
+++ exercise_2.js
@@ -39,34 +39,48 @@ function isVowel(char) {
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
-
+	var vowels = ["a", "e", "i", "o", "u"];
+	var saying = ("Go get them");
 function translate(phrase){
-    //...
-}
+	var newSaying = saying.filter(function(currentLetter){
+		if (currentLetter === vowels) {
+			return []
+		} else {
+			return currentLetter + "o" + currentletter;
+		};
+	});
+    
+};
 
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
-    //...
+function reverse(str) {
+    return str.split("").reverse().join("");
 }
 
 // ---------------------
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
 // ---------------------
+var words = ["important", "coffee", "tank", "gun"];
 
 function findLongestWord(words){
-    //...
-}
+	words.filter(function(currentWord) {
+		var long = currentWord.length > 10;
+		return long;
+	});
+};
 
 // ---------------------
 // Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
 // ---------------------
-
+var words = ["important", "coffee", "tank", "gun", "i"];
 function filterLongWords(words, i){
-    //...
+    words.filter(function(currentWord, i) {
+    	var longer = currentWord.length > "i";
+    });
 }
 
 // ---------------------
@@ -74,5 +88,9 @@ function filterLongWords(words, i){
 // ---------------------
 
 function charFreq(string){
-    //...
-}
+    var string = [aaaaabbbbcccac];
+    string.split("").forEach(function(letter) {
+    	count[a]; count[b]; count[c];
+    	return count
+    });
+};

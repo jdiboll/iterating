d0c6d48cef1e885f2bcecc539a73d4a0a9cd385e
FIRST_NAME LAST_NAME <YOUR_EMAIL>
2016-03-02 22:38:52 -0500

    halfway through excercise_2

--- exercise_2.js
+++ exercise_2.js
@@ -2,31 +2,45 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
 
-function max(){
-    //...
-}
+function max(x,y) {
+    if (x > y) {
+    	return "true";
+    } else {
+    	return "false";
+    };
+};
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // ---------------------
 
-function maxOfThree(){
-    //...
-}
+function maxOfThree(x,y,z) {
+    if (x > y > z) {
+    	return "x";
+    } else if (x < y > z) {
+    	return "y";
+    } else {
+    	return "z";
+    };
+};
 
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
 
-function isVowel(char){
-    //...
-}
+function isVowel(char) {
+	if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+		return "true";
+	} else {
+		return "false";
+	};
+};
 
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function rovarspraket(phrase){
+function translate(phrase){
     //...
 }
 
